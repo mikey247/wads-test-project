071877736647ed3130ebab12882b84df80351199
louise.lever <louise.lever@manchester.ac.uk>
2017-04-01 19:02:18 +0100

    Created blocks.py for appropriate block code. Updated html5 skeleton to include pygment CSS for code highlighting

--- sitecore/blocks.py
+++ sitecore/blocks.py
@@ -1,6 +1,13 @@
 from django.utils.functional import cached_property
+from django.utils.safestring import mark_safe
 from django import forms
+
 from wagtail.wagtailcore import blocks
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import get_formatter_by_name
+
 from sitecore.validators import ValidateShortcodes
 
 
@@ -12,3 +19,44 @@ class ShortcodeRichTextBlock(blocks.RichTextBlock):
         return forms.CharField(validators=[ValidateShortcodes],widget=get_rich_text_editor_widget(self.editor), **self.field_options)
 
 
+class BSCodeBlock(blocks.StructBlock):
+    """
+    Code highlighting block, using pygments library
+    """
+    LANGUAGE_CHOICES = (
+        ('python', 'Python'),
+        ('javascript', 'JavaScript'),
+        ('bash', 'Bash'),
+    )
+
+    lang = blocks.ChoiceBlock(choices=LANGUAGE_CHOICES)
+    code = blocks.TextBlock(required=True)
+    line_nums = blocks.BooleanBlock(required=False, help_text='Check to include line numbers')
+
+    class Meta:
+        icon = 'fa fa-code'
+        
+
+    def render(self, value, context=None):
+        src = value['code'].strip('\n')
+        lang = value['lang']
+        linenos = value['line_nums']
+
+        pyg_lexer = get_lexer_by_name(lang)
+        pyg_formatter = get_formatter_by_name('html', linenos=False, cssclass='codehilite', style='default', noclasses=False)
+        
+        return mark_safe(highlight(src, pyg_lexer, pyg_formatter))
+
+
+class BSBlockquoteBlock(blocks.StructBlock):
+    """
+    Block for supporting full Bootstrap 3 <blockquote> markup
+    """
+    quote = ShortcodeRichTextBlock(required=True)
+    footer = blocks.CharBlock(required=False)
+    cite = blocks.CharBlock(required=False)
+    reverse = blocks.BooleanBlock(required=False, help_text='Check to right-align the quote')
+
+    class Meta:
+        icon = 'fa fa-quote-left'
+        template = 'bootstrapblocks/blockquote.html'
