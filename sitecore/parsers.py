83ed5990e77f4fbd7a45df3831f3361870979778
louise.lever <louise.lever@manchester.ac.uk>
2017-04-04 23:57:10 +0100

    Dropped use of sitesettings for shortcode delimiters as context not available to validator calls (value only) so impossible to retrieve site/context info. Have used simple config.py file to define, and re-use in both places we need create a parser object. Not pretty but works. Updated tag use and templates. Changed delims to single [ and ] and using /code rather than endcode as syntax.

--- sitecore/parsers.py
+++ sitecore/parsers.py
@@ -1,18 +1,17 @@
 import shortcodes
+import sitecore.config as sitecore_config
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
-from sitecore.models import SiteSettings
 
-def ParseShortcodes(value, context):
-    settings = context['settings']['sitecore']['sitesettings']
-    parser = shortcodes.Parser(start=settings.shortcode_start, end=settings.shortcode_end, esc=settings.shortcode_esc)
+
+def ParseShortcodes(value):
+    parser = shortcodes.Parser(start=sitecore_config.START, end=sitecore_config.END, esc=sitecore_config.ESC)
     try:
-        return mark_safe(parser.parse(mark_safe(value), context))
+        return mark_safe(parser.parse(mark_safe(value)))
     except shortcodes.InvalidTagError:
         raise ValidationError(
             _('Invalid shortcode tag(s) detected, please correct.'),
             params={'value':value},
         )
-
-
+    
--- sitecore/templatetags/shortcodes.py
+++ sitecore/templatetags/shortcodes.py
@@ -1,29 +1,33 @@
 import shortcodes
 import html
 from django import template
+import sitecore.config as sitecore_config
 from sitecore.parsers import ParseShortcodes
 
 ### create shortcodes
 
-@shortcodes.register("code", "endcode")
+@shortcodes.register("code", "/code")
 def handler(context, content, pargs, kwargs):
     code = html.escape(content)
     return '<code>%s</code>' % (code)
 
 @shortcodes.register("kbd")
 def handler(context, content, pargs, kwargs):
-    try:
-        kbd = html.escape(pargs[0])
-    except:
-        kbd = "No key specified"
-    return '<kbd>%s</kbd>' % (kbd)
-
-parser = shortcodes.Parser(start="[[", end="]]", esc="\\")
+    result = []
+    for arg in pargs:
+        try:
+            kbd = html.escape(arg)
+        except:
+            kbd = "No key specified"
+        result.append('<kbd>%s</kbd>' % (kbd))
+    return u' '.join(result)
 
 # create simple_tag (we need context to get parser settings)
 
+parser = shortcodes.Parser(start=sitecore_config.START, end=sitecore_config.END, esc=sitecore_config.ESC)
+
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
-def shortcodes(context, value):
-    return ParseShortcodes(value, context)
+@register.simple_tag()
+def shortcodes(value):
+    return ParseShortcodes(value)
