9d1752fe80aefe61b58b5ea0fab816b0cd8a5073
louise.lever <louise.lever@manchester.ac.uk>
2017-04-05 00:26:05 +0100

    Added shortcodes for var, samp and abbr

--- sitecore/templatetags/shortcodes.py
+++ sitecore/templatetags/shortcodes.py
@@ -8,8 +8,18 @@ from sitecore.parsers import ParseShortcodes
 
 @shortcodes.register("code", "/code")
 def handler(context, content, pargs, kwargs):
-    code = html.escape(content)
-    return '<code>%s</code>' % (code)
+    content_html = html.escape(content)
+    return '<code>%s</code>' % (content_html)
+
+@shortcodes.register("samp", "/samp")
+def handler(context, content, pargs, kwargs):
+    content_html = html.escape(content)
+    return '<samp>%s</samp>' % (content_html)
+
+@shortcodes.register("var", "/var")
+def handler(context, content, pargs, kwargs):
+    content_html = html.escape(content)
+    return '<var>%s</var>' % (content_html)
 
 @shortcodes.register("kbd")
 def handler(context, content, pargs, kwargs):
@@ -22,7 +32,18 @@ def handler(context, content, pargs, kwargs):
         result.append('<kbd>%s</kbd>' % (kbd))
     return u' '.join(result)
 
-# create simple_tag (we need context to get parser settings)
+@shortcodes.register("abbr")
+def handler(context, content, pargs, kwargs):
+    try:
+        title = html.escape(pargs[0])
+        abbr = html.escape(pargs[1])
+    except:
+        title = "Invalid abbreviation"
+        
+    return '<abbr title="%s">%s</abbr>' % (title,abbr)
+
+
+# create simple_tag (cleaner processing as we can mark_safe in code rather than a filter chain)
 
 parser = shortcodes.Parser(start=sitecore_config.START, end=sitecore_config.END, esc=sitecore_config.ESC)
 
