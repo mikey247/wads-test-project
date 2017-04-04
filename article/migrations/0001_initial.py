03a067b29d97c6ca8ea13d59c6c726a04b37969d
louise.lever <louise.lever@manchester.ac.uk>
2017-04-04 17:38:49 +0100

    Removing migrations from repo.

--- article/migrations/0001_initial.py
+++ article/migrations/0001_initial.py
@@ -8,7 +8,7 @@ import modelcluster.contrib.taggit
 import modelcluster.fields
 import sitecore.blocks
 import sitecore.fields
-import sitecore.validators
+import sitecore.parsers
 import wagtail.wagtailcore.blocks
 import wagtail.wagtailcore.fields
 import wagtail.wagtailimages.blocks
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
                 ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
                 ('author', models.CharField(max_length=255)),
                 ('date', models.DateField(verbose_name='Post date')),
-                ('intro', sitecore.fields.ShortcodeRichTextField(blank=True, validators=[sitecore.validators.ValidateShortcodes, sitecore.validators.ValidateShortcodes, sitecore.validators.ValidateShortcodes])),
+                ('intro', sitecore.fields.ShortcodeRichTextField(blank=True, validators=[sitecore.parsers.ParseShortcodes, sitecore.parsers.ParseShortcodes, sitecore.parsers.ParseShortcodes])),
                 ('body', wagtail.wagtailcore.fields.StreamField((('heading', wagtail.wagtailcore.blocks.CharBlock(classname='full title')), ('paragraph', sitecore.blocks.ShortcodeRichTextBlock()), ('image', wagtail.wagtailimages.blocks.ImageChooserBlock()), ('email', wagtail.wagtailcore.blocks.EmailBlock())))),
             ],
             options={
