1e638df594540b502c9376c97129b5a09d119107
louise.lever <louise.lever@manchester.ac.uk>
2017-04-05 18:55:02 +0100

    Extend samp shortcode to allow a 'block' argument to wrap multilines in pre block. Abbr will raise RenderingError if arguments insufficient (not great).

--- sitecore/templatetags/shortcodes.py
+++ sitecore/templatetags/shortcodes.py
@@ -1,7 +1,7 @@
 import shortcodes
 import html
 from django import template
-import sitecore.config as sitecore_config
+import sitecore.config as cfg
 from sitecore.parsers import ParseShortcodes
 
 ### create shortcodes
@@ -13,8 +13,19 @@ def handler(context, content, pargs, kwargs):
 
 @shortcodes.register("samp", "/samp")
 def handler(context, content, pargs, kwargs):
-    content_html = html.escape(content)
-    return '<samp>%s</samp>' % (content_html)
+    try:
+        mode = html.escape(pargs[0])
+        if mode is 'block':
+            enable_pre = True
+        else:
+            enable_pre = False
+    except:
+        enable_pre = False
+    samp_html = '<samp>%s</samp>' % (content)
+    if enable_pre:
+        return '<pre>%s</pre>' % (samp_html)
+    else:
+        return samp_html
 
 @shortcodes.register("var", "/var")
 def handler(context, content, pargs, kwargs):
@@ -38,14 +49,14 @@ def handler(context, content, pargs, kwargs):
         title = html.escape(pargs[0])
         abbr = html.escape(pargs[1])
     except:
-        title = "Invalid abbreviation"
+        raise RenderingError
         
     return '<abbr title="%s">%s</abbr>' % (title,abbr)
 
 
 # create simple_tag (cleaner processing as we can mark_safe in code rather than a filter chain)
 
-parser = shortcodes.Parser(start=sitecore_config.START, end=sitecore_config.END, esc=sitecore_config.ESC)
+parser = shortcodes.Parser(start=cfg.START, end=cfg.END, esc=cfg.ESC)
 
 register = template.Library()
 
