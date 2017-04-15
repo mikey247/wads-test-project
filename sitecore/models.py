5ec9d2845ec5353244dd634e08e3ab4d5a099924
louise.lever <louise.lever@manchester.ac.uk>
2017-04-15 10:26:08 +0100

    Removed shortcodes settings from site settings model and interface.

--- sitecore/models.py
+++ sitecore/models.py
@@ -31,28 +31,6 @@ class SiteSettings(BaseSetting):
     # Social media settings
     twitter = models.URLField(blank=True, help_text='Twitter Account')
 
-    # Shortcodes settings
-    shortcode_start = models.CharField(
-        blank=False,
-        default='[[',
-        max_length = 2,
-        help_text='Define the shortcode start delimiter e.g. [['
-    )
-
-    shortcode_end = models.CharField(
-        blank=False,
-        default=']]',
-        max_length = 2,
-        help_text='Define the shortcode end delimiter e.g. ]]'
-    )
-
-    shortcode_esc = models.CharField(
-        blank=False,
-        default='\\\\',
-        max_length = 2,
-        help_text='Define the shortcode escape code e.g. \\'
-    )
-
     # create the panels
     theme_tab_panel = [
         FieldPanel('bootstrap_theme'),
@@ -62,15 +40,8 @@ class SiteSettings(BaseSetting):
         FieldPanel('twitter'),
     ]
 
-    shortcode_tab_panel = [
-        FieldPanel('shortcode_start'),
-        FieldPanel('shortcode_end'),
-        FieldPanel('shortcode_esc'),
-    ]
-
     # Combine into tabbed panel interface
     edit_handler = TabbedInterface([
         ObjectList(theme_tab_panel, heading='Theme'),
         ObjectList(social_tab_panel, heading='Social Media'),
-        ObjectList(shortcode_tab_panel, heading='Shortcodes'),
     ])
