c7756e5d9ddd598a5497704e09d87c38f68fd8ed
louise.lever <louise.lever@manchester.ac.uk>
2017-05-13 22:36:11 +0100

    Paginated the article index page. Article now derived from SitePage to get shared tag cluster. Uses CoreBlock for streamfield.

--- article/models.py
+++ article/models.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
  
@@ -10,64 +12,81 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel, Inl
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailsearch import index
 
-from sitecore.fields import ShortcodeRichTextField
 from sitecore import blocks as sitecore_blocks
+from sitecore.fields import ShortcodeRichTextField
+from sitecore.models import SitePage
 
 from modelcluster.fields import ParentalKey
 from modelcluster.tags import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
+
 class ArticleIndexPage(Page):
-    intro = RichTextField(blank=True)
+    desc = ShortcodeRichTextField(blank=True)
+    per_page = models.PositiveSmallIntegerField(default=10,
+                                                validators=[
+                                                    MinValueValidator(1),
+                                                    MaxValueValidator(100)
+                                                ])
 
     def get_context(self, request):
         # Update content to include only published posts; ordered by reverse-chronological
         context = super(ArticleIndexPage, self).get_context(request)
-        articles = self.get_children().live().order_by('-first_published_at')
+        all_articles = self.get_children().live().order_by('-first_published_at')
+
+        paginator = Paginator(all_articles, self.per_page) 
+
+        page = request.GET.get('page')
+        try:
+            articles = paginator.page(page)
+        except PageNotAnInteger:
+            articles = paginator.page(1)
+        except EmptyPage:
+            articles= paginator.page(paginator.num_pages)
+
         context['articles'] = articles
+
         return context
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full"),
+        FieldPanel('desc', classname="full"),
+        FieldPanel('per_page'),
     ]
 
 
-class ArticlePageTag(TaggedItemBase):
-    content_object = ParentalKey('ArticlePage', related_name='tagged_items')
+#class ArticlePageTag(TaggedItemBase):
+#    content_object = ParentalKey('article.ArticlePage', related_name='tagged_articles')
 
 
-class ArticlePage(Page):
+class ArticlePage(SitePage):
     author = models.CharField(max_length=255)
     date = models.DateField("Post date")
-    tags = ClusterTaggableManager(through=ArticlePageTag, blank=True)
+#    tags = ClusterTaggableManager(through=PageTag, blank=True)
     intro = ShortcodeRichTextField(blank=True)
 
-    body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title")),
-        ('paragraph', sitecore_blocks.ShortcodeRichTextBlock()),
-        ('image', ImageChooserBlock()),
-        ('email', blocks.EmailBlock()),
-        ('blockquote', sitecore_blocks.BSBlockquoteBlock()),
-        ('code', sitecore_blocks.BSCodeBlock()),
-    ])
+    body = StreamField(sitecore_blocks.CoreBlock)
 
-    search_fields = Page.search_fields + [
+    search_fields = SitePage.search_fields + [
         index.SearchField('author'),
+        index.SearchField('date'),
+        index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
-    api_fields = [
+    api_fields = SitePage.search_fields + [
+        'author',
         'date',
+#        'tags',
+        'intro',
         'body',
-        'tags'
     ]
 
-    content_panels = Page.content_panels + [
+    content_panels = SitePage.content_panels + [
         MultiFieldPanel([
             FieldPanel('author'),
             FieldPanel('date'),
-            FieldPanel('tags'),
+#            FieldPanel('tags'),
             FieldPanel('intro'),
-        ], heading="Blog Information"),
+        ], heading="Article Information"),
         StreamFieldPanel('body'),
     ]
