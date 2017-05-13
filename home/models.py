7ee4c0e29f14dc123c7d3970872c03e3dddf4d1c
louise.lever <louise.lever@manchester.ac.uk>
2017-05-13 22:33:57 +0100

    Home page uses article model. Sets transparency for navbar on splash image (which is still hardcoded). Article rendering still needs templatetag.

--- home/models.py
+++ home/models.py
@@ -2,14 +2,24 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
 from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailsearch import index
 
+from sitecore import blocks as sitecore_blocks
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    body = StreamField(sitecore_blocks.CoreBlock)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    api_fields = [
+        'body',
+    ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        StreamFieldPanel('body'),
     ]
