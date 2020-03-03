4297766401fa6652fd881f5bff85a549fc543e94
louiselever <louise.lever@manchester.ac.uk>
2020-03-03 20:29:44 +0000

    Added new SearchIndexPage to better handle free text search queries

--- sitecore/models.py
+++ sitecore/models.py
@@ -5,6 +5,7 @@ a superclass SitePage model to share commonalities, and support for tag indexing
 :Copyright: Research IT, IT Services, The University of Manchester
 """
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db import models
 from django.template.response import TemplateResponse
 
@@ -13,9 +14,11 @@ from wagtail.contrib.routable_page.models import route, RoutablePageMixin
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.core.models import Page, Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search.models import Query
 
 from modelcluster.fields import ParentalKey
 from modelcluster.tags import ClusterTaggableManager
+
 from taggit.models import Tag, TaggedItemBase
 
 from sitecore import constants
@@ -211,3 +214,44 @@ class TagIndexPage(RoutablePageMixin, Page):
             self.get_context(request, slug=slug)
         )
 
+
+
+class SearchIndexPage(Page):
+    """
+    This defines a search index page for searching content with given search terms
+    The ?query= field in the page request is used to search for specific
+    content matching the terms. As the superclass SitePage is used, content is found across all
+    derived models.
+    """
+    
+    def get_context(self, request, slug=None):
+        context = super(SearchIndexPage, self).get_context(request)
+
+        search_query = request.GET.get('query', None)
+        page = request.GET.get('page', 1)
+
+        # Retrieve all pages that match search terms
+        # Get first query matched and add a hit?
+        if search_query:
+            search_results = SitePage.objects.live().specific().order_by('-first_published_at').search(search_query)
+            query = Query.get(search_query)
+        
+            # Record hit
+            query.add_hit()
+        else:
+            search_results = SitePage.objects.none()
+
+        # Pagination
+        paginator = Paginator(search_results, 10)
+        try:
+            search_results = paginator.page(page)
+        except PageNotAnInteger:
+                search_results = paginator.page(1)
+        except EmptyPage:
+            search_results = paginator.page(paginator.num_pages)
+            
+        # Return all matching pages and whole tag cloud
+        context['pages'] = search_results
+        context['query'] = search_query
+        
+        return context
