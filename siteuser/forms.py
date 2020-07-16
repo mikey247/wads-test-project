05410d43f9e689c0da56c8421c62f1e1771761f1
Josh Woodcock <joshua.woodcock@manchester.ac.uk>
2020-07-16 14:49:50 +0100

    added 'required=False' to Twitter handles. This closes issue #11

--- siteuser/forms.py
+++ siteuser/forms.py
@@ -6,10 +6,10 @@ from wagtail.users.forms import UserEditForm, UserCreationForm
 
 class CustomUserEditForm(UserEditForm):
     country = forms.CharField(required=True, label=_("Country"))
-    twitter = forms.CharField(label=_('Twitter handle'))
+    twitter = forms.CharField(required=False, label=_('Twitter handle'))
 
 
 class CustomUserCreationForm(UserCreationForm):
     country = forms.CharField(required=True, label=_("Country"))
-    twitter = forms.CharField(label=_('Twitter handle'))
+    twitter = forms.CharField(required=False, label=_('Twitter handle'))
                         
