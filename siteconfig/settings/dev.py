4f834e96746828f0092b058cd7cbe06326964d02
louiselever <louise.lever@manchester.ac.uk>
2020-10-20 17:36:35 +0100

    Removed dev/production only packages (moved to dev/production.py)

--- siteconfig/settings/dev.py
+++ siteconfig/settings/dev.py
@@ -5,6 +5,16 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# DEV ONLY modifications
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
 try:
     from .local import *
 except ImportError:
--- siteconfig/settings/production.py
+++ siteconfig/settings/production.py
@@ -2,6 +2,13 @@ from .base import *
 
 DEBUG = False
 
+INSTALLED_APPS += [
+    'pg_copy',
+]
+
+MIDDLEWARE += [
+]
+
 try:
     from .local import *
 except ImportError:
