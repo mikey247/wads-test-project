5f6fe23ba187fdd00d4b4ae04ad5cbb5dd0ab1c1
Josh Woodcock <joshua.woodcock@manchester.ac.uk>
2021-02-18 14:58:32 +0000

    added DEFAULT_FROM_EMAIL setting to the dev local.py template

--- siteconfig/settings/template_dev_local.py
+++ siteconfig/settings/template_dev_local.py
@@ -5,5 +5,7 @@ DO NOT COMMIT THE local.py FILE TO THE REPOSITORY
 See https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-SECRET_KEY = '<INSERT SECRET KEY HERE>'
-WAGTAIL_SITE_NAME = '<INSERT SITE NAME HERE>'
+SECRET_KEY = 'w2@j2$*)e0ad$-k18w9rds1^gwdj)4^=#h=a2)h+n3v8hai(yh'
+WAGTAIL_SITE_NAME = 'wagtailcore dev'
+
+DEFAULT_FROM_EMAIL = f'{WAGTAIL_SITE_NAME} - <no-reply@manchester.ac.uk>'
