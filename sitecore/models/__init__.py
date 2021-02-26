7f715775bb30a59e41e7e50253aec94a10e0c5c5
louiselever <louise.lever@manchester.ac.uk>
2021-02-26 18:02:05 +0000

    Moved CaptionImage to SiteImage and sitecore app

--- sitecore/models/__init__.py
+++ sitecore/models/__init__.py
@@ -2,3 +2,4 @@ from .settings import SiteSettings
 from .sitepage import SitePageTags, SitePage
 from .search_index import SearchIndexPage
 from .tag_index import TagIndexPage
+from .siteimage import SiteImage, CustomRendition
