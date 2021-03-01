b197093b078941bb75991a1d626e55cb88beb887
Josh Woodcock <joshua.woodcock@manchester.ac.uk>
2021-03-01 14:17:53 +0000

    removed the inserted site key and site name

--- siteconfig/settings/template_dev_local.py
+++ siteconfig/settings/template_dev_local.py
@@ -5,7 +5,7 @@ DO NOT COMMIT THE local.py FILE TO THE REPOSITORY
 See https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-SECRET_KEY = 'w2@j2$*)e0ad$-k18w9rds1^gwdj)4^=#h=a2)h+n3v8hai(yh'
-WAGTAIL_SITE_NAME = 'wagtailcore dev'
+SECRET_KEY = '<INSERT KEY_HERE>'
+WAGTAIL_SITE_NAME = '<INSERT WAGTAIL_SITE_NAME_HERE>'
 
 DEFAULT_FROM_EMAIL = f'{WAGTAIL_SITE_NAME} - <no-reply@manchester.ac.uk>'
