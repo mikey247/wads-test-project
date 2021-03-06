4cd90c96bbb0a88fdb1c0e2569d8fb0eea26d621
louiselever <louise.lever@manchester.ac.uk>
2021-03-06 17:33:15 +0000

    LDAP signals only loaded if ENABLE_LDAP

--- sitecore/apps.py
+++ sitecore/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class SitecoreConfig(AppConfig):
     name = 'sitecore'
 
     def ready(self):
-        import sitecore.signals
+        if settings.ENABLE_LDAP:
+            import sitecore.signals
