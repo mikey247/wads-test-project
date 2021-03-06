c0b167cdca7681067ee95fc7fdfd91a3caa0beb6
louiselever <louise.lever@manchester.ac.uk>
2021-03-06 17:32:40 +0000

    Debug Toolbar only loaded if ENABLE_DEBUG_TOOLBAR

--- siteconfig/urls.py
+++ siteconfig/urls.py
@@ -28,12 +28,13 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    import debug_toolbar
     
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    if settings.ENABLE_DEBUG_TOOLBAR:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
