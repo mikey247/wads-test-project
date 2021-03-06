bf9d5739514fd5bad5fb43c57e4d82fdc1328ba6
louiselever <louise.lever@manchester.ac.uk>
2021-03-06 17:31:33 +0000

    Debug Toolbar only loaded if ENABLE_DEBUG_TOOLBAR

--- siteconfig/settings/dev.py
+++ siteconfig/settings/dev.py
@@ -5,17 +5,19 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# DEV ONLY modifications
-
-INSTALLED_APPS += [
-    'debug_toolbar',
-]
-
-MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-
 try:
     from .local import *
 except ImportError:
     pass
+
+# DEV ONLY modifications
+
+if ENABLE_DEBUG_TOOLBAR:
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
+    
+    MIDDLEWARE += [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ]
+
