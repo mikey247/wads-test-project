bbc0144f787b83a6c6e57c77b6f5bc8cf04380e0
louiselever <louise.lever@manchester.ac.uk>
2021-03-06 17:32:06 +0000

    Cleaner dev setup. Added ENABLE_xyz features

--- siteconfig/settings/template_dev_local.py
+++ siteconfig/settings/template_dev_local.py
@@ -5,7 +5,86 @@ DO NOT COMMIT THE local.py FILE TO THE REPOSITORY
 See https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-SECRET_KEY = '<INSERT KEY_HERE>'
+# wads-wagtail Optional Features
+# ------------------------------------------------------------------------
+
+ENABLE_LDAP = False # True|False
+ENABLE_DEBUG_TOOLBAR = False # True|False
+
+# Wagtail Site Settings
+# ------------------------------------------------------------------------
+# Base URL to use when referring to full URLs within the Wagtail admin
+# backend - e.g. in notification emails.
+# Don't include '/admin' or a trailing slash
+
 WAGTAIL_SITE_NAME = '<INSERT WAGTAIL_SITE_NAME_HERE>'
+BASE_URL = '<INSERT BASE_URL HERE>' # usually localhost for dev
+
+# Security
+# ------------------------------------------------------------------------
+
+SECRET_KEY = '<INSERT KEY_HERE>'
+
+# Email
+# ------------------------------------------------------------------------
 
 DEFAULT_FROM_EMAIL = f'{WAGTAIL_SITE_NAME} - <no-reply@manchester.ac.uk>'
+
+# Database
+# ------------------------------------------------------------------------
+# Use Postgresql by default rather than sqlite3
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<INSERT DATABASE NAME HERE>',
+        'USER': '<INSERT USER NAME HERE>',
+        'PASSWORD': '<INSERT PASSWORD NAME HERE>',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# Hosts
+# ------------------------------------------------------------------------
+# Hosts/domain names that are valid for this site
+# REQUIRED if DEBUG is False
+# WSL2: if not serving on localhost:8000 add IP from running `ifconfig`
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+# Language
+# ------------------------------------------------------------------------
+
+LANGUAGE_CODE = 'en-gb'
+
+# Search Backends
+# ------------------------------------------------------------------------
+# See: https://docs.wagtail.io/en/v2.7/topics/search/backends.html#backends
+# If using postgresql (now default db) use its search features
+
+WAGTAILSEARCH_BACKENDS = {
+    'default': {
+        'BACKEND': 'wagtail.contrib.postgres_search.backend',
+        'INDEX': '<INSERT SITE SPECIFIC INDEX NAME HERE>',
+    },
+}
+
+# REST Framework
+# ------------------------------------------------------------------------
+# See: https://www.django-rest-framework.org/
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+}
