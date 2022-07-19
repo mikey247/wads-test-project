a429cacd676d18721fc837c40df61cbd6e33004e
BenitoMatischen <benito.matischen@manchester.ac.uk>
2022-07-19 10:36:47 +0100

    Remove redundant file

--- fixtures/scripts/database.00.init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE gitissues;
-CREATE USER brooke WITH PASSWORD 'brooke';
-ALTER ROLE brooke SET default_transaction_isolation TO 'read committed';
-ALTER ROLE brooke SET client_encoding TO 'utf8';
-ALTER ROLE brooke SET timezone TO 'utc';
-GRANT ALL PRIVILEGES ON DATABASE gitissues TO brooke;
