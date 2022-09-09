7949851666ccccaa95e1e2c91367360e766c1ae2
Annie Zheng <qinhan.zheng@student.manchester.ac.uk>
2022-09-09 11:29:27 +0100

    Edit html and form for date filter

--- article/forms.py
+++ article/forms.py
@@ -1,12 +1,20 @@
 from django import forms
+from datetime import datetime
 
 class FilterForm(forms.Form):
     selected_date = forms.DateField(
         required=False,
-        widget=forms.DateInput(
+        widget=forms.SelectDateWidget(
             attrs={
                 'id': 'selected_date',
                 'type': 'date'
-            }
+            },
+            empty_label=("Year", "Month", "Day"),
+            years=[]
         )
     )
+
+    def __init__(self, *args, **kwargs):
+        super(FilterForm, self).__init__(*args, **kwargs)
+        YEAR = reversed(range(2000, (datetime.now().year+1)))
+        self.fields['selected_date'].widget.years = YEAR
