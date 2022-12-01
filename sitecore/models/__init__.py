443715576b6c5661bad616236edb0bb58d02b87b
Josh Woodcock <joshua.woodcock@manchester.ac.uk>
2022-12-01 15:59:56 +0000

    rempved the siteform as it doesn't exist

--- sitecore/models/__init__.py
+++ sitecore/models/__init__.py
@@ -1,6 +1,5 @@
 from .search_index import SiteSearchIndexPage
 from .settings import SiteSettings
-from .siteform import AbstractFormSetting
 from .siteimage import SiteImage, SiteRendition
 from .sitepage import SitePageTags, SitePage
 from .tag_index import SiteTagIndexPage
