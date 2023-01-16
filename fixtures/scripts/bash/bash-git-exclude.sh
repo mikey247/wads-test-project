c97f48e8123beb42a49736a5fa34c277bad2ecef
Josh Woodcock <joshua.woodcock@manchester.ac.uk>
2023-01-16 17:47:00 +0000

    simple git exclude script and python environment script

---
+++ fixtures/scripts/bash/bash-git-exclude.sh
@@ -0,0 +1,6 @@
+cat >> .git/info/exclude << EOF
+*-venv
+*-env
+.vscode
+__pycache__
+EOF
---
+++ fixtures/scripts/bash/bash-python-env.sh
@@ -0,0 +1,4 @@
+virtualenv wagtail-env 
+source wagtail-env/bin/activate
+pip install -r requirements.00.core.txt
+pip install -r requirements.01.dev.txt
