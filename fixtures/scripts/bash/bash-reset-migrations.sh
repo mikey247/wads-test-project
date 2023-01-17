be9240804d17cb395dfcc0e3cc7b87753c9b335b
Josh Woodcock <joshua.woodcock@manchester.ac.uk>
2023-01-17 10:31:49 +0000

    removed the function to make it harder to accidentally trigger

--- fixtures/scripts/bash/bash-reset-migrations.sh
+++ fixtures/scripts/bash/bash-reset-migrations.sh
@@ -1,9 +1,5 @@
-function reset_migrations() {
-
-    rm article/migrations/0*
-    rm event/migrations/0*
-    rm home/migrations/0*
-    rm sitecore/migrations/0*
-    rm siteuser/migrations/0*
-
-}
+rm article/migrations/0*
+rm event/migrations/0*
+rm home/migrations/0*
+rm sitecore/migrations/0*
+rm siteuser/migrations/0*
